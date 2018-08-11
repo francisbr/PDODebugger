6adb3139d8f9d307db0331b6a8a71614ce0c6771
Francis <francisbouletrouleau@hotmail.com>
2018-08-10 21:33:54 -0400

    Complet project rename to PDODebugger

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'SQLDebugger'`, async(() => {
+  it(`should have as title 'PDODebugger'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('SQLDebugger');
+    expect(app.title).toEqual('PDODebugger');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SQLDebugger!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to PDODebugger!');
   }));
 });
